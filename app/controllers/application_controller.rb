1dc2b0be79ddf11ac0f3acdcaf1950b1779fc77f
John Martirano <johnmartirano@gmail.com>
2011-04-16 19:16:57 -0500

    Css tweaks, created blogroll via sidebar2_title[_area,_body]. @posts is available globally because blogroll is available globally.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :load_posts
+
   def index
   end
+
+  def load_posts
+    @posts=Post.all
+  end
+
 end
